9359d89c5fbd0cceef4e3ec071fad2b5892b9c19
Noor <fnoor@activision.com>
2017-07-06 16:25:02 -0700

    Cleaning up script, started creating classes

--- steamMain.py
+++ steamMain.py
@@ -17,37 +17,66 @@ def getKey(path):
 		raise Exception("Not valid file path")
 
 apiKey = getKey( "C:\\personal\\steam.key" )
-api = WebAPI( apiKey )
+
 
 ##########
 
-# set profileURL
-profileURL = "https://steamcommunity.com/id/kaf/"
-myProfileID = steam.steamid.steam64_from_url(profileURL)
+class getSteamID():
+	""""Class to retrieve steam64 ID via name, URL"""
+	def fromName( self, profileName ):
+		url = ('https://steamcommunity.com/id/{0}/').format(profileName)
+		return self.fromURL(url)
 
-### get user profile info!
-#works!
-myInfo = api.ISteamUser.GetPlayerSummaries(steamids = myProfileID)
-profileDict = myInfo['response']['players']
+	def fromURL( self, url ):
+		return steam.steamid.steam64_from_url(url)
 
-# for x in profileDict: 
-# 	for k,v in x.items():
-# 		print k,v
+	def fromID( self, id ):
+		return id
+
+############
+
+class getProfileInfo():
+	"""Fetch user profile info"""
+	def __init__(self, userID):
+
+		self.userID = userID
+		self.friendIDs = []
+		self.rawData = api.ISteamUser.GetPlayerSummaries(steamids = userID)
+
+	def rawProfile( self ):
+
+		'''Get profile info cleanly from rawData'''
+		self.rawProfile = self.rawData['response']['players'][0]
+	 	return self.rawProfile
+		
+	
+	def friends( self ):
+		friendRaw = api.ISteamUser.GetFriendList(steamid= self.userID)
+		for k,v in friendRaw.items():
+			for x in v.items(): friendRaw = x[1]
+		
+		return friendRaw
+
+
+##############
+
+api = WebAPI( apiKey )
+
+getSteamID = getSteamID()
+userID = getSteamID.fromName('kaf')
+
+getProfileInfo = getProfileInfo(userID)
+
+# print getProfileInfo.rawProfile()
+friends =  getProfileInfo.friends()
+
+for friend in friends:
+
+	friendID = friend['steamid']
+	friend = api.ISteamUser.GetPlayerSummaries(steamids = friendID)
+	friend = friend['response']['players'][0]
+
+	print friend['personaname']
+	
 	
 
-#### friends stuff ######
-
-myFriendsInfo =  api.ISteamUser.GetFriendList(steamid= myProfileID)
-
-for key,value in myFriendsInfo.items():
-	for x in value.items(): 
-		myFriendsInfo = x[1]
-
-#myFriendsInfo just returns steamid, friends_since and relationship
-
-for item in myFriendsInfo:	
-	friendProfile = api.ISteamUser.GetPlayerSummaries(steamids = item['steamid'])
-	friendProfile = friendProfile['response']['players'][0]
-		print friendProfile['personaname'], friendProfile['steamid']
-
-	
